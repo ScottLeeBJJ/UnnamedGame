d1548c83d8f6ab7ac08a9182aead362725f7e5d6
Scott Lee <56131751+ScottLeeBJJ@users.noreply.github.com>
2023-02-27 16:36:20 -0600

    Creating BallLaunchScript
    C++ script for player to spawn in a ball to launch at the targets.

---
+++ Source/UnnamedGame/BallSpawnScript.h
@@ -0,0 +1,15 @@
+// This script will allow the player to spawn a ball to launch at the target
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class UNNAMEDGAME_API BallSpawnScript
+{
+public:
+	BallSpawnScript();
+	~BallSpawnScript();
+};
